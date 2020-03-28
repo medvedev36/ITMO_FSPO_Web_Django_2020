3e4e2063a72dca49b2531b1714c18b51f297d6ab
Екатерина Агапова <katena.agapova.01@mail.ru>
2020-03-28 13:21:11 +0300

    Сделала практическую работу №2

--- students/y2333/practical_works/agapova_ekaterina/simple_django_web_project/project_first_app/urls.py
+++ students/y2333/practical_works/agapova_ekaterina/simple_django_web_project/project_first_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import CarList
 
 
 urlpatterns = [
     path('owner/<int:id>/', views.details),
+    path('ownerlist', views.owner_list),
+    path('carlist', CarList.as_view())
 ]
--- students/y2333/practical_works/agapova_ekaterina/simple_django_web_project/project_first_app/views.py
+++ students/y2333/practical_works/agapova_ekaterina/simple_django_web_project/project_first_app/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render
-from .models import Owner
+from .models import Owner, Car
+from django.views.generic.list import ListView
 
 
 def details(request, id):
@@ -9,4 +10,15 @@ def details(request, id):
         owner = Owner.objects.get(pk=id)
     except Owner.DoesNotExist:
         raise Http404("Owner doesn't exist")
-    return render(request, 'details.html', {'owner': owner})
+    return render(request, 'project_first_app/owner_details.html', {'owner': owner})
+
+
+def owner_list(request):
+
+    owners = Owner.objects.all()
+    return render(request, 'project_first_app/owner_list.html', {'owners': owners})
+
+
+class CarList(ListView):
+
+    model = Car
