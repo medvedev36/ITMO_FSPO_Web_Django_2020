43b716dd652de67a5ec033e8f6f8d4d416a5e730
Anisimov Arthur <anisimov.arthur@gmail.com>
2020-04-03 14:21:16 +0300

    Добавил практическую работу 2

--- students/y2335/practical_works/Anisimov/django_project_Anisimov/project_first_app/urls.py
+++ students/y2335/practical_works/Anisimov/django_project_Anisimov/project_first_app/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from .views import CarList
+from .views import CarCreate
 
 from . import views
+from . import forms
 
 urlpatterns = [
      path('owner/<int:owner_id>/', views.details),
+     path('owners/', views.listOwners),
+     path('ownerview/', views.ownerView),
+     path('cars/', CarList.as_view()),
+     path('carform/', CarCreate.as_view() ),
  ]
