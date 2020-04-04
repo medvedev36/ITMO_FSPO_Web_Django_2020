cd22a8b2f5c0fb5546923f0ed79dacce9c80b4f0
AlexMakhotkin <54326502+AlexMakhotkin@users.noreply.github.com>
2020-04-04 14:30:47 +0300

    Добавлена практическая работа № 2

--- students/y2335/practical_works/Makhotkin_Alexander/lesson2_11022020/makhotkin_app/urls.py
+++ students/y2335/practical_works/Makhotkin_Alexander/lesson2_11022020/makhotkin_app/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('owner/<int:owner_id>', views.detail),
+    path('list_owners/', views.list_owners),
+    path('list_cars/', views.CarsList.as_view()),
+    path('owner_form/', views.owner_view),
+    path('car_form/', views.CarCreate.as_view()),
 ]
