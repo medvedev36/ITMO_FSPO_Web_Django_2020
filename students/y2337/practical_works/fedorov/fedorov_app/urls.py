274457fb9803b15f8ffe826f68c7f3bf3b942a8a
Fedorov Ilya <loghorrean74@gmail.com>
2020-04-10 20:19:08 +0300

    Практическая работа 2

--- students/y2337/practical_works/fedorov/fedorov_app/urls.py
+++ students/y2337/practical_works/fedorov/fedorov_app/urls.py
@@ -4,4 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('owners/<int:id>', details),
+    path('car/list/', CarList.as_view()),
+    path('owner/<int:owner_id>/', details),
+    path('owner/list/', owner_list),
 ]
--- students/y2337/practical_works/fedorov/fedorov_app/views.py
+++ students/y2337/practical_works/fedorov/fedorov_app/views.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from django.http import Http404
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
 # Create your views here.
 from fedorov_app.models import *
@@ -12,4 +13,13 @@ def details(request, id):
         owner = Owner.objects.get(pk=id)
     except Owner.DoesNotExist:
         return Http404
-    return render(request, "owners/detail.html", {'owner': owner})
+    return render(request, "owner/detail.html", {'owner': owner})
+
+
+def owner_list(request):
+    return render(request, 'owner/list.html', {'owners': Owner.objects.all()})
+
+
+class CarList(ListView):
+    model = Car
+    template_name = 'car/list.html'
