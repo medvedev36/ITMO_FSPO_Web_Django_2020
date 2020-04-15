22c82230b6c95063af55c905d29f9274ae730253
NGriin <NGriin@github.com>
2020-04-15 17:53:32 +0300

    Revert "Практическа"
    This reverts commit c7d5fa1c092e9472784d1d3bcb5e086c04f7d337.

--- students/y2336/practical_works/Grinzayd/Pr1/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Student\PycharmProjects\Lession_2_bond\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
