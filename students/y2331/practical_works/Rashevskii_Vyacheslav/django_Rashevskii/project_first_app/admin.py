f0310c284f72e896140d02e27928efd79447b717
Slavaski <slava121231@mail.ru>
2020-05-15 13:51:44 +0300

    Выполнена практическая работа №3

--- students/y2331/practical_works/Rashevskii_Vyacheslav/django_Rashevskii/project_first_app/admin.py
+++ students/y2331/practical_works/Rashevskii_Vyacheslav/django_Rashevskii/project_first_app/admin.py
@@ -3,6 +3,7 @@ from project_first_app.models import Owner
 from project_first_app.models import Car
 from project_first_app.models import DriverLicense
 from project_first_app.models import Ownership
+from project_first_app.models import User
 
 
 class Admin(admin.ModelAdmin):
@@ -13,3 +14,4 @@ admin.site.register(Owner, Admin)
 admin.site.register(Car, Admin)
 admin.site.register(DriverLicense, Admin)
 admin.site.register(Ownership, Admin)
+admin.site.register(User, Admin)
