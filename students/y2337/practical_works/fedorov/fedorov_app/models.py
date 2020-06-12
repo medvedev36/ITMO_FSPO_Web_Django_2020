eabcd891c206411de2c13d3ba5d465893f304f1f
Fedorov Ilya <loghorrean74@gmail.com>
2020-06-12 16:25:14 +0300

    Практическая работа 4

--- students/y2337/practical_works/fedorov/fedorov_app/models.py
+++ students/y2337/practical_works/fedorov/fedorov_app/models.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    passport = models.IntegerField(unique=True, null=True)
+    address = models.CharField(max_length=70, null=True)
+    nationality = models.CharField(max_length=48, null=True)
 
 
 class Car(models.Model):
@@ -15,14 +22,15 @@ class Owner(models.Model):
     first_name = models.CharField(max_length=30)
     second_name = models.CharField(max_length=30)
     birthday = models.DateField()
-    sex = models.CharField(max_length=1, choices = SEXES)
+    sex = models.CharField(max_length=1, choices=SEXES)
     car = models.ManyToManyField(Car, through='OwnerShip')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
 
 
 class CarLicense(models.Model):
     TYPES = [('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D')]
     # id = models.IntegerField()
-    type = models.CharField(max_length=1, choices = TYPES)
+    type = models.CharField(max_length=1, choices=TYPES)
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
 
 
