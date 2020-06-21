f93a305e177155ef5b59352ebdb6ea484568a5e7
Nikita Khmelev <inthedarkness77@gmail.com>
2020-06-21 14:46:24 +0300

    Добавлены практические работы №1,№2,№3. Хмелёв Никита Y2334

---
+++ students/y2334/practical_works/Khmelev_Nikita/Pr1+Pr2/khmelev/khmelev_app/views.py
@@ -0,0 +1,37 @@
+from django.http import Http404
+from django.shortcuts import render
+from khmelev_app.models import Owner,Car
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from .forms import OwnerForm
+
+def details(request, owner_id):
+    try:
+       owner = Owner.objects.get(pk=owner_id)
+    except Owner.DoesNotExist:
+        raise Http404("Owner does not exist")
+    return render(request, 'owner/details.html', {'owner': owner})
+
+def list_owner(request):
+    context ={}
+
+    context["dataset"] = Owner.objects.all()
+    return render(request, "owner/list_owners.html", context)
+
+class  CarList(ListView):
+    template_name = "owner/list_cars.html"
+    model = Car
+
+def create_owner(request):
+    context = {}
+    form = OwnerForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context['form'] = form
+    return render(request,"owner/create_owner.html", context)
+
+class CarCreate(CreateView):
+    template_name = "owner/create_car.html"
+    success_url = "."
+    model = Car
+    fields = ['mark', 'model', 'year']
