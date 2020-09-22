2d49f190f3d707d871164c98666f5fec1baf9032
vessau <veronikaspb2017@mail.ru>
2020-09-22 20:06:47 +0300

    Добавлена практическая работа 2, Жилина Вероника Y2435

--- students/y2335/practical_works/Zhilina_Veronika/Lab1_3/zhilina_app/urls.py
+++ students/y2335/practical_works/Zhilina_Veronika/Lab1_3/zhilina_app/urls.py
@@ -3,5 +3,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('owners/<int:owner_id>/', views.detail)
+    path('owners/<int:owner_id>/', views.detail),
+    path('owners/', views.allOwners),
+    path('autos/', views.AutoList.as_view()),
+    path('createOwner/', views.createOwner),
+    path('createAuto/', views.AutoCreate.as_view()),
 ]
