d7bc4535662544850d1ea9a4369726b335063342
IgorKonevsky <igorkonevsky@gmail.com>
2020-11-01 03:17:22 +0300

    everything deleted

--- students/y2331/laboratory_works/Konevsky_Igor/furs/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Farm, Fur, Coat, Purchase
-
-admin.site.register(Farm)
-admin.site.register(Fur)
-admin.site.register(Coat)
-admin.site.register(Purchase)
